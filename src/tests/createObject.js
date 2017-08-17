ed792b15f176918a49bce099b71c3af685f65447
Дмитрий <dmitriypak1986@mail.ru>
2017-08-17 12:44:42 +0500

    added file

---
+++ src/tests/createObject.js
@@ -0,0 +1,13 @@
+
+export default createObject = {
+    createByNew: function (){
+        let obj = new Object()
+    },
+    createByBrekets: function (){
+        let obj = {}
+    },
+    createByFunction: function (){
+        let obj = createObject()
+    }
+
+}
